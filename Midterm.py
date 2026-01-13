a9eac2cf8dad0b2bf402ca114d8209c6e1434f72
LilianeAr <xdbsvzj9kc@privaterelay.appleid.com>
2026-01-13 11:04:58 -0500

    Midterm.py
    Implement hollow right triangle function and print results.

--- Midterm.py
+++ Midterm.py
@@ -6,9 +6,28 @@ def area_of_circle(radius):
     return round(result, 2)
 print(area_of_circle(5))
 
+
 # Q2: Hollow Right Triangle
 def hollow_right_triangle(n):
     return ""
+
+# Q2: Hollow Right Triangle
+def hollow_right_triangle(n):
+    result = ""
+
+    for i in range(n):
+        for j in range(n):
+            if i == n - 1 or j == 0 or j == i:
+                result += "*"
+            else:
+                result += " "
+        result += "\n"
+
+    return result.rstrip()
+print(hollow_right_triangle(3))
+print(hollow_right_triangle(4))
+print(hollow_right_triangle(5))
+
 
 # Q3: Inverted Pyramid
 def inverted_pyramid(n):
